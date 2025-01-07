0b0d46ae63b3338317b09ddef1cf3da7aacd4303
nogsposito <viniciussposito3103@gmail.com>
2025-01-07 11:52:39 -0300

    exercicio

--- curso_programacao/src/Main.java
+++ curso_programacao/src/Main.java
@@ -8,34 +8,46 @@ public class Main {
         
         Scanner sc = new Scanner(System.in);
 
-        int x = sc.nextInt();
-        int y = sc.nextInt();
-        int z = sc.nextInt();
+        double tri1a, tri1b, tri1c;
+        double tri2a, tri2b, tri2c;
 
-        int higher = max(x, y, z);
+        System.out.println("Digite os lados do primeiro triângulo: ");
+        tri1a = sc.nextDouble();
+        tri1b = sc.nextDouble();
+        tri1c = sc.nextDouble();
+        
+        System.out.println("Digite os lados do segundo triângulo: ");
+        tri2a = sc.nextDouble();
+        tri2b = sc.nextDouble();
+        tri2c = sc.nextDouble();
 
-        showResult(higher);
+        int biggest = maior(tri1a, tri1b, tri1c, tri2a, tri2b, tri2c);
+
+        System.out.println("MAIOR: TRIANGULO " + biggest);
         
         sc.close();
 
     }
 
-    public static int max (int a, int b, int c){
+    public static int maior(double tri1a, double tri1b, double tri1c, double tri2a, double tri2b, double tri2c){
         
-        int aux;
-
-        if (a > b && a > c){
-            aux = a;
-        } else if (b > a && b > c){
-            aux = b;
-        } else {
-            aux = c;
+        double area1, area2;
+        double p1, p2;
+        
+        p1 = ((tri1a + tri1b + tri1c)/2);
+        area1 = Math.sqrt(p1*(p1-tri1a)*(p1-tri1b)*(p1-tri1c));
+        
+        p2 = ((tri2a + tri2b + tri2c)/2);
+        area2 = Math.sqrt(p2*(p2-tri2a)*(p2-tri2b)*(p2-tri2c));
+        
+        System.out.println("Area do 1: " + area1);
+        System.out.println("Area do 2: " + area2);
+        
+        if (area1 > area2){
+            return 1;
+        }else{
+            return 2;
         }
 
-        return aux;
-    }
-
-    public static void showResult(int value){
-        System.out.println("Higher: " + value);
     }
 }
